c8f79240d124ad17154154620fd2959d7b0e5473
Or Peled <orppeled@gmail.com>
2013-12-19 17:24:25 +0200

    Finished besides social

--- wp-content/themes/preen/index.php
+++ wp-content/themes/preen/index.php
@@ -10,7 +10,9 @@
 								<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
 									
 									<header class="article-header">
-										<?php the_post_thumbnail( 'bones-thumb-270' ); ?>
+                                        <div class="image-cropper-mobile">
+										    <?php the_post_thumbnail( 'bones-thumb-270' ); ?>
+                                        </div>
 										<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 										<p class="byline vcard"><?php
 											printf( __( '<time class="updated" datetime="%1$s" pubdate>%2$s</time><span>|</span>By <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link());
